a7cb2478f945144b308e8b017dfba6af2ffd6a2e
Igor Efrimidi <pikeouterwear@gmail.com>
2020-04-02 00:02:07 +0500

    Add new directory

---
+++ js/main.js
@@ -0,0 +1,31 @@
+let startBtn = document.getElementById('start'),
+    budgetValue = document.getElementsByClassName('daybudget-value'),
+    levelValue = document.getElementsByClassName('level-value'),
+    expensesValue = document.getElementsByClassName('expenses-value'),
+    optionalExpensesValue = document.getElementsByClassName('optionalexpenses-value'),
+    incomeValue = document.getElementsByClassName('income-value'),
+    monthSavingsValue = document.getElementsByClassName('monthsavings-value'),
+    yearSavingsValue = document.getElementsByClassName('yearsavings-value'),
+
+
+    expensesItem = document.getElementsByClassName('expenses-item'),
+    expensesBtn = document.getElementsByTagName('button'),
+    optionalExpensesBtn = document.getElemensByTagName('button'),
+    countBudgetBtn = document.getElemensByTagName('button'),
+    optionalExpensesItem = document.querySelectorAll('.optionalexpenses-item'),
+    incomeItem = document.querySelector('.choose-income'),
+    checkSavings = document.querySelector('#savings'),
+    sumValue = document.querySelector('.choose-sum'),
+    percentValue = document.querySelector('.choose-percent'),
+    yearValue = document.querySelector('.year-value'),
+    monthValue = document.querySelector('.month-value'),
+    dayValue = document.querySelector('.day-value');
+    
+
+
+
+
+
+
+
+
